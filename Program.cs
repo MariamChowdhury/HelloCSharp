2f4b61472c27cc150583d9a93d2467e298fe3cf9
mariamchowdhury.200@gmail.com <mariamchowdhury.200@gmail.com>
2021-10-31 00:29:26 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            try
+            {
+                var num = 1211124;
+                byte num2 = Convert.ToByte(num);
+                System.Console.WriteLine(num2);
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("Error !!!");
+            }
+        }
+    }
+}
